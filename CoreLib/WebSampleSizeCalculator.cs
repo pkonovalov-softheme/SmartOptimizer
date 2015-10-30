710c39ee0573de12b86c4d7a1badea9062ee1ee5
Pavel Konovalov <pavel.konovalov@softheme.com>
2015-10-30 15:25:42 +0200

    Add WebSampleSizeCalculator

---
+++ CoreLib/WebSampleSizeCalculator.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Statistics.Distributions.Univariate;
+
+namespace CoreLib
+{
+    public sealed class WebSampleSizeCalculator
+    {
+        private readonly double _baseConvertionRate;
+        private readonly double _targetConvertionRateLift;
+        private readonly double _confidenceLevel;
+        private readonly double _statisticalPower;
+        private readonly int _numberOfTests;
+        private readonly long _visitorsPerDay;
+        private readonly bool _bonferroniCorrectionEnabled;
+        private long _samplesPerTest;
+
+
+        public WebSampleSizeCalculator(
+            double baseConvertionRate,
+            double targetConvertionRateLift,
+            double confidenceLevel,
+            double statisticalPower,
+            int numberOfTests,
+            long visitorsPerDay,
+            bool bonferroniCorrectionEnabled
+            )
+        {
+            _baseConvertionRate = baseConvertionRate;
+            _targetConvertionRateLift = targetConvertionRateLift;
+            _confidenceLevel = confidenceLevel;
+            _statisticalPower = statisticalPower;
+            _numberOfTests = numberOfTests;
+            _visitorsPerDay = visitorsPerDay;
+            _bonferroniCorrectionEnabled = bonferroniCorrectionEnabled;
+        }
+
+        /// <summary>
+        /// 1 - Alpha
+        /// </summary>
+        public double ConfidenceLevel
+        {
+            get
+            {
+                if (_bonferroniCorrectionEnabled)
+                {
+                    double correctedConflevel = 1 - ((1 - _confidenceLevel) / (_numberOfTests - 1));
+                    return correctedConflevel;
+                }
+
+                return _confidenceLevel;
+            }
+        }
+
+        public double ConfidenceThreshold
+        {
+            get
+            {
+                NormalDistribution dist = new NormalDistribution(0, 1);
+                double result = dist.InverseDistributionFunction(1 - (1 - ConfidenceLevel) / 2);
+                return result;
+            }
+        }
+        
+
+        /// <summary>
+        /// 1 - Beta
+        /// </summary>
+        public double StatisticalPower
+        {
+            get { return _statisticalPower; }
+        }
+
+        public double StatisticalPowerThreshold
+        {
+            get
+            {
+                NormalDistribution dist = new NormalDistribution(0, 1);
+                double result = dist.InverseDistributionFunction(1 - (1 - StatisticalPower));
+                return result;
+            }
+        }
+
+        public double BaseConvertionRate
+        {
+            get { return _baseConvertionRate; }
+        }
+
+        public double ConvertionRateAbsoluteDifference
+        {
+            get
+            {
+                return BaseConvertionRate * TargetConvertionRateLift;
+            }
+        }
+
+        public double ExpectedConvertionRate
+        {
+            get
+            {
+                return BaseConvertionRate *  (1 + TargetConvertionRateLift);
+            }
+        }
+
+        public double TargetConvertionRateLift
+        {
+            get { return _targetConvertionRateLift; }
+        }
+
+        public long SamplesPerTest
+        {
+            get { return _samplesPerTest; }
+            set { _samplesPerTest = value; }
+        }
+
+        public void CalculateSamples()
+        {
+            double result =
+                          BaseConvertionRate *
+                          (
+                                (1 - BaseConvertionRate) + 
+                                (1 + TargetConvertionRateLift) * (1 - (1 + TargetConvertionRateLift) * BaseConvertionRate)
+                          ) *
+                          Math.Pow(ConfidenceThreshold + StatisticalPowerThreshold, 2) /
+                          Math.Pow(BaseConvertionRate - BaseConvertionRate*(1 + TargetConvertionRateLift), 2);
+
+            _samplesPerTest = Convert.ToInt64(Math.Floor(result));
+        }
+    }
+}
---
+++ CoreTests/CoreTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CoreLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTests
+{
+    [TestClass]
+    public class WebSampleSizeCalculatorTests
+    {
+        [TestMethod]
+        public void SampleSize_Test_1()
+        {
+            WebSampleSizeCalculator calc = new WebSampleSizeCalculator(0.118, 0.075, 0.95, 0.8, 5, 10000, false);
+            calc.CalculateSamples();
+            Assert.AreEqual(calc.SamplesPerTest, 21529);
+        }
+
+        [TestMethod]
+        public void SampleSize_Test_2()
+        {
+            WebSampleSizeCalculator calc = new WebSampleSizeCalculator(0.182, 0.063, 0.90, 0.83, 5, 10000, false);
+            calc.CalculateSamples();
+            Assert.AreEqual(calc.SamplesPerTest, 15666);
+        }
+
+        [TestMethod]
+        public void SampleSize_Test_BonferroniCorrection()
+        {
+            WebSampleSizeCalculator calc = new WebSampleSizeCalculator(0.182, 0.063, 0.90, 0.83, 5, 10000, true);
+            calc.CalculateSamples();
+            Assert.AreEqual(calc.SamplesPerTest, 23683);
+        }
+
+        [TestMethod]
+        public void SampleSize_Test_AbsoluteDifference()
+        {
+            WebSampleSizeCalculator calc = new WebSampleSizeCalculator(0.182, 0.063, 0.90, 0.83, 5, 10000, false);
+            calc.CalculateSamples();
+            double delta = calc.ConvertionRateAbsoluteDifference - 0.0115;
+            Assert.IsTrue(delta < 0.001);
+        }
+
+        [TestMethod]
+        public void SampleSize_Test_ExpectedConvertionRate()
+        {
+            WebSampleSizeCalculator calc = new WebSampleSizeCalculator(0.182, 0.063, 0.90, 0.83, 5, 10000, false);
+            calc.CalculateSamples();
+            double delta = calc.ExpectedConvertionRate - 0.1935;
+            Assert.IsTrue(delta < 0.001);
+        }
+    }
+}
