9e58d733d4a75883c9ab7fe613a1f5188ca691df
Pavel Konovalov <pavel.konovalov@softheme.com>
2015-11-11 14:10:03 +0200

    save

---
+++ CoreLib/OptimizationTartget.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public sealed class OptimizationTartget
+    {
+        private readonly int _unitsCount;
+
+        /// <summary>
+        /// unitsCount - count of units for the optimization
+        /// </summary>
+        /// <param name="unitsCount"></param>
+        public OptimizationTartget(int unitsCount)
+        {
+            _unitsCount = unitsCount;
+            CurrectUnit = 0;
+        }
+
+        public int UnitsCount
+        {
+            get { return _unitsCount; }
+        }
+
+        public int CurrectUnit { get; set; }
+    }
+}
